3095e561f3560bdb8c9f3a3a116787414d1e5150
Rizart Dokollari <r.dokollari@gmail.com>
2019-11-26 20:15:06 +0000

    Introduce pre-processor director, headers, std namespace, cout, and endl new line.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+
 int main()
 {
+
+    std::cout << "You hurry up, and move into the data center room.\n";
+    std::cout << "Your heart is racing, and the adrenaline is racing through "
+                 "your veins." << std::endl;
+    std::cout << "Some of the machines in front you have USB sockets.\n";
+    std::cout << "Taking out your dev machine from your backpack, it takes you"
+                 " a couple of tries to plug it in; your hand is trembling from"
+                 "the stress.\n";
+    std::cout << "Finally, with your trusty Arch Linux OS, you're ready.\n";
+
     return 0;
 }
