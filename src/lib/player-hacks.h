b4f888cd6753ed6e5e39b1833dd9be4d3d91975b
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-15 22:42:21 +0100

    Extract return value from prompt function

--- src/lib/player-hacks.h
+++ src/lib/player-hacks.h
@@ -3,22 +3,20 @@
 
 #endif //TRIPLE_X_GAME_PROMPT_PLAYER_HACK_H
 
-std::tuple<int, int, int> PromptHack(int MatrixSum, int MatrixProduct) {
-    int alpha_code, beta_code, gamma_code;
-
-    std::cout << "Code Review\n";
+void PromptHack(int MatrixSum, int MatrixProduct) {
+    std::cout << "Code Matrix\n";
     std::cout << "===========\n";
     std::cout << "✖ There are 3 numbers in the code\n";
     std::cout << "✖ They add up to: " << MatrixSum << "\n";
     std::cout << "✖ The codes multiply to: " << MatrixProduct << "\n\n";
+}
 
-    std::cout << "Enter the three codes:\n";
+std::tuple<int, int, int> GetPlayerHack() {
+    int alpha_code, beta_code, gamma_code;
     std::cin >> alpha_code >> beta_code >> gamma_code;
-    std::cout << "You responded with: " << alpha_code << ", ";
-    std::cout << beta_code << ", and " << gamma_code << "\n\n";
-
     return std::make_tuple(alpha_code, beta_code, gamma_code);
 }
+
 
 bool HasValidHackAttempt(int alpha_code, int beta_code, int gamma_code, int matrix_sum, int matrix_product) {
     const int product = alpha_code * beta_code * gamma_code;
