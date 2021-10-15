9df6129e05f3d6d22c64fada47f0a1f8073686af
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-15 21:53:28 +0100

    Extract story intro

---
+++ src/lib/print-story-intro.h
@@ -0,0 +1,19 @@
+#ifndef TRIPLE_X_GAME_INTRODUCT_STORY_H
+#define TRIPLE_X_GAME_INTRODUCT_STORY_H
+
+#endif //TRIPLE_X_GAME_INTRODUCT_STORY_H
+
+void PrintStoryIntro() {
+    std::cout << "Entering TripleX\n";
+    std::cout << "================\n";
+
+    std::cout << "You hurry up, and move into the data center room.\n";
+    std::cout << "Your heart is racing, and the adrenaline is racing through "
+                 "your veins."
+              << std::endl;
+    std::cout << "Some of the machines in front you have USB sockets.\n";
+    std::cout << "Taking out your dev machine from your backpack, it takes you"
+                 " a couple of tries to plug it in; your hand is trembling "
+                 "from the stress.\n";
+    std::cout << "Finally, with your trusty Arch Linux OS, you're ready.\n\n";
+}
