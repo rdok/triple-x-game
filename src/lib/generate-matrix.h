a972ea6e845312febbea94ea4cb83146bb23756a
Rizart Dokollari <r.dokollari@gmail.com>
2021-10-16 16:53:40 +0100

    Refactor underscored local variables

--- src/lib/generate-matrix.h
+++ src/lib/generate-matrix.h
@@ -3,10 +3,10 @@
 
 #endif //TRIPLE_X_GAME_GENERATE_MATRIX_H
 
-std::tuple<int, int> generateMatrix(int Difficulty) {
-    const int alpha_code = rand() % Difficulty + Difficulty;
-    const int beta_code = rand() % Difficulty + Difficulty;
-    const int gemma_code = rand() % Difficulty + Difficulty;
+std::tuple<int, int> generateMatrix(int difficulty_multiplier) {
+    const int alpha_code = rand() % difficulty_multiplier + difficulty_multiplier;
+    const int beta_code = rand() % difficulty_multiplier + difficulty_multiplier;
+    const int gemma_code = rand() % difficulty_multiplier + difficulty_multiplier;
 
     const int matrix_sum = alpha_code + beta_code + gemma_code;
     const int matrix_product = alpha_code * beta_code * gemma_code;
